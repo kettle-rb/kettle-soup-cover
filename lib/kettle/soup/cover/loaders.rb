6b7e3ef9afad3e9e20bde269d2b48251b1f1acd7
Peter H. Boling <peter.boling@gmail.com>
2026-03-20 15:08:59 -0600

    ♻️ AST > Regex

--- lib/kettle/soup/cover/loaders.rb
+++ lib/kettle/soup/cover/loaders.rb
@@ -7,18 +7,18 @@ module Kettle
         def load_formatters
           SimpleCov.formatters = Kettle::Soup::Cover::Constants::FORMATTERS
             .each_with_object([]) do |fmt_data, formatters|
-            require fmt_data[:lib].to_s
+              require fmt_data[:lib].to_s
 
-            klass = SimpleCov::Formatter.const_get(fmt_data[:klass])
+              klass = SimpleCov::Formatter.const_get(fmt_data[:klass])
 
-            if fmt_data[:type] == :lcov
-              klass.config do |c|
-                c.report_with_single_file = true
-                c.single_report_path = "#{Kettle::Soup::Cover::Constants::COVERAGE_DIR}/lcov.info"
+              if fmt_data[:type] == :lcov
+                klass.config do |c|
+                  c.report_with_single_file = true
+                  c.single_report_path = "#{Kettle::Soup::Cover::Constants::COVERAGE_DIR}/lcov.info"
+                end
               end
-            end
 
-            formatters << klass
+              formatters << klass
           end
         end
 
